c0d2f8947ef64d4e8daee7e56c32709c1436e041
Amaan <amaans1912@gmail.com>
2023-04-20 16:14:01 +0530

    Updates

---
+++ precache-manifest.d1247dc0bfcdd1a19d6351a61965f600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe71dff637c4240f9b12",
+    "url": "/textmoditor-react/static/css/main.8e51d5d8.chunk.css"
+  },
+  {
+    "revision": "fe71dff637c4240f9b12",
+    "url": "/textmoditor-react/static/js/main.fe71dff6.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/textmoditor-react/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "feef1cc2e97e37e8ead4",
+    "url": "/textmoditor-react/static/js/2.feef1cc2.chunk.js"
+  },
+  {
+    "revision": "e7604cd2624a3f602a2c",
+    "url": "/textmoditor-react/static/js/runtime~main.e7604cd2.js"
+  },
+  {
+    "revision": "fe3e8ef5020db9798a95221eb4c318f4",
+    "url": "/textmoditor-react/index.html"
+  }
+];
